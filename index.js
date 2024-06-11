ed8388e9553311adeffb7455b7eeb870332fc7f4
lgaieta.dev <lgaieta.dev@gmail.com>
2024-06-11 00:26:49 +0000

    Removed unnecesary things from index

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const express = require('express');
-const path = require('path')
-
-const app = express();
-
-const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
-
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
-});
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`)
 })

dfa14ee4875e0e13f4d9ebeb4da42e01ef89a42a
lgaieta.dev <lgaieta.dev@gmail.com>
2024-06-17 18:05:28 +0000

    Added express json middleware

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import { UserRegisterRouter } from "./src/routes/UserRegisterRouter.js";
 const app = express();
 const port = 5000;
 
+app.use(express.json());
 app.use(UserRegisterRouter);
 
 app.get("/", (request, response) => {
-    response.send("Hello Home!");
+    response.send("Hello world!");
 });
 
 app.listen(port, () => {

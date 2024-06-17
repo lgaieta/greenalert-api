4a6a088c934c7b06731546de0fdacc24a3a8e30a
lgaieta.dev <lgaieta.dev@gmail.com>
2024-06-17 22:06:47 +0000

    Removed vercel things and added dotenv package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { UserRegisterRouter } from "./src/routes/UserRegisterRouter.js";
 import { PORT } from "./src/config.js";
+import "dotenv/config";
 
 const app = express();
 
@@ -16,5 +17,3 @@ app.get("/", (request, response) => {
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
-
-export default app;

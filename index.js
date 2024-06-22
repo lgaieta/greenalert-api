874aa914ad72fbac98b28de5ec24053c96ce011c
lgaieta.dev <lgaieta.dev@gmail.com>
2024-06-22 22:24:07 +0000

    Replaced UserRegisterRouter with UserRouter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { UserRegisterRouter } from "./src/routes/UserRegisterRouter.js";
+import { UserRouter } from "./src/routes/UserRouter.js";
 import { PORT } from "./src/config.js";
 import "dotenv/config";
 
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(UserRegisterRouter);
+app.use("/user", UserRouter);
 
 app.get("/", (request, response) => {
     response.send("Hello world!");
--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -2,9 +2,9 @@ import express from "express";
 import registerUser from "../usecases/registerUser.js";
 import MySQLUserRepository from "../services/MySQLUserRepository.js";
 
-const UserRegisterRouter = express.Router();
+const UserRouter = express.Router();
 
-UserRegisterRouter.post("/register", (request, response) => {
+UserRouter.post("/register", (request, response) => {
     const { email, password } = request.body;
 
     registerUser({
@@ -15,4 +15,4 @@ UserRegisterRouter.post("/register", (request, response) => {
     response.send("User created");
 });
 
-export { UserRegisterRouter };
+export { UserRouter };

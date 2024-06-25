cff39755f225395b2f6146a6227e43d41a57d12b
lgaieta.dev <lgaieta.dev@gmail.com>
2024-06-25 17:42:02 +0000

    Added morgan

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import { UserRouter } from "./src/routes/UserRouter.js";
 import { PORT } from "./src/config.js";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(cors());
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/user", UserRouter);

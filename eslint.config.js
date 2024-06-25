c448133d7b53224a27076ac88ac9060674313f8e
lgaieta <lgaieta.dev@gmail.com>
2024-06-25 10:41:18 -0300

    Added prettier eslint plugin and changed global to node

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    eslintPluginPrettierRecommended,
 ];

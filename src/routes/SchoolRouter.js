f21453ea92de3e566b358d6e3781d36fe9dac36d
lgaieta <lgaieta.dev@gmail.com>
2024-10-15 17:23:58 +0000

    Added admin validation at post school

--- src/routes/SchoolRouter.js
+++ src/routes/SchoolRouter.js
@@ -1,17 +1,29 @@
 import express from "express";
 import MySQLSchoolRepository from "../services/MySQLSchoolRepository.js";
+import SessionManager from "../services/SessionManager.js";
+import jwt from "jsonwebtoken";
+const { JsonWebTokenError } = jwt;
 
 const SchoolRouter = express.Router();
 
 SchoolRouter.post("/", async (request, response) => {
     const { cue, name, locality } = request.body;
-
-    if (!cue || !name || !locality) return response.status(403).send("Error");
+    if (!cue || !name || !locality) return response.status(400).send("Error");
+    const { access_token } = request.cookies;
 
     try {
+        const { usertype } = await SessionManager.verifyToken(
+            access_token || "",
+        );
+
+        if (usertype !== "admin")
+            return response.status(403).send("Unauthorized");
+
         await MySQLSchoolRepository.save({ cue, name, locality });
         return response.status(200).send("Created successfully!");
     } catch (error) {
+        if (error instanceof JsonWebTokenError)
+            return response.status(403).send("Unauthorized");
         console.log(error);
         return response.status(400).send("Error");
     }

2c8a980660ce21e8c84a1e95eb27a7eb0c2e82fb
lgaieta <lgaieta.dev@gmail.com>
2024-10-15 17:31:57 +0000

    Added student authorization in report post

--- src/routes/ReportRouter.js
+++ src/routes/ReportRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import MySQLReportRepository from "../services/MySQLReportRepository.js";
+import SessionManager from "../services/SessionManager.js";
 
 const ReportRouter = express.Router();
 
@@ -8,6 +9,12 @@ ReportRouter.post("/", async (request, response) => {
 
     if (!lat || !lng) return response.status(400).send("Error");
     try {
+        const { usertype } = await SessionManager.verifyToken(
+            request.cookies.access_token || "",
+        );
+        if (usertype !== "student")
+            return response.status(403).send("Unauthorized");
+
         await MySQLReportRepository.save({ description, lat, lng });
     } catch (error) {
         console.error(error);

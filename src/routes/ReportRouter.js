4a2ca7a47cafaff4e50f88d452239cef749c71e4
lgaieta <lgaieta.dev@gmail.com>
2024-10-22 11:34:39 -0300

    Added save report with new data

--- src/routes/ReportRouter.js
+++ src/routes/ReportRouter.js
@@ -5,19 +5,43 @@ import SessionManager from "../services/SessionManager.js";
 const ReportRouter = express.Router();
 
 ReportRouter.post("/", async (request, response) => {
-    const { description, lat, lng } = request.body;
+    const { description, lat, lng, email, courseId, type, locality } =
+        request.body;
 
-    if (!lat || !lng) return response.status(400).send("Error");
+    if (!lat || !lng || !email || !courseId || !type || !locality)
+        return response.status(400).send("Error");
+
     try {
         const { usertype } = await SessionManager.verifyToken(
             request.cookies.access_token || "",
         );
+
         if (usertype !== "student")
             return response.status(403).send("Unauthorized");
 
-        await MySQLReportRepository.save({ description, lat, lng });
+        await MySQLReportRepository.save({
+            description,
+            lat,
+            lng,
+            email,
+            courseId,
+            type,
+            locality,
+        });
+
+        return response.status(200).send("Created successfully!");
     } catch (error) {
         console.error(error);
+        return response.status(400).send("Error");
+    }
+});
+
+ReportRouter.get("/", async (request, response) => {
+    try {
+        const reportList = await MySQLReportRepository.list();
+        return response.json(reportList);
+    } catch (error) {
+        console.log(error);
         return response.status(400).send("Error");
     }
 });
--- src/services/MySQLReportRepository.js
+++ src/services/MySQLReportRepository.js
@@ -3,10 +3,28 @@ import { pool } from "./pool.js";
 class MySQLReportRepository {
     static async save(report) {
         const result = await pool.query(
-            "INSERT INTO report (desc_report, latitude, length) VALUES (?, ?, ?)",
-            [report.description, report.lat, report.lng],
+            "INSERT INTO report (desc_report, latitude, length, user_email, course_id, type_report, locality) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [
+                report.description,
+                report.lat,
+                report.lng,
+                report.email,
+                report.courseId,
+                report.type,
+                report.locality,
+            ],
         );
         return result[0];
+    }
+
+    static async list() {
+        const [result] = await pool.query("SELECT * FROM report");
+        console.log(result);
+        return result.map((row) => ({
+            description: row.desc_report,
+            lat: row.latitude,
+            lng: row.length,
+        }));
     }
 }
 

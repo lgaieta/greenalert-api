62857fa67e4b1732bf776e280ead483071716b3f
lgaieta <lgaieta.dev@gmail.com>
2024-11-11 23:05:00 -0300

    Refactored UserRouter

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -11,132 +11,39 @@ import { ADMIN_EMAIL, ADMIN_PASSWORD } from "../config.js";
 
 const UserRouter = express.Router();
 
-UserRouter.get("/", async (request, response) => {
-    const token = request.cookies.access_token;
+// Authentication Routes
+UserRouter.post("/login", loginHandler);
+UserRouter.get("/validate", validateHandler);
 
-    if (!token) return response.status(403).send("Unauthorized access");
+// User Routes
+UserRouter.post("/register", registerHandler);
 
-    console.log(token);
+// Director Routes
+UserRouter.get("/director", getDirectorsHandler);
+UserRouter.post("/director/register", registerDirectorHandler);
 
-    response.send("Protected users!");
-});
+// Professor Routes
+UserRouter.get("/professor", getProfessorsHandler);
+UserRouter.post("/professor/register", registerProfessorHandler);
 
-UserRouter.get("/director", async (request, response) => {
-    const token = request.cookies.access_token;
-
-    if (!token) return response.status(403).send("Unauthorized access");
-
+async function loginHandler(request, response) {
     try {
-        const payload = await SessionManager.verifyToken(token);
-        if (payload.usertype !== "admin")
-            return response.status(403).send("Unauthorized access");
-        const directorList = await MySQLUserRepository.listDirectors();
-        return response.json(directorList);
-    } catch (error) {
-        console.log(error);
-        return response.status(400).send("Error");
-    }
-});
+        const { email, password } = request.body;
+        if (!email || !password) return response.status(400).send("Error");
 
-UserRouter.get("/professor", async (request, response) => {
-    const token = request.cookies.access_token;
+        if (email === ADMIN_EMAIL && password === ADMIN_PASSWORD) {
+            const token = await SessionManager.generateToken({
+                email,
+                usertype: "admin",
+            });
 
-    if (!token) return response.status(403).send("Unauthorized access");
+            return response
+                .cookie("access_token", token, {
+                    secure: process.env.NODE_ENV === "production",
+                })
+                .send("Logged in successfully");
+        }
 
-    try {
-        const payload = await SessionManager.verifyToken(token);
-        if (payload.usertype !== "director")
-            return response.status(403).send("Unauthorized access");
-        const professorList = await MySQLUserRepository.listProfessors();
-        return response.json(professorList);
-    } catch (error) {
-        console.log(error);
-        return response.status(400).send("Error");
-    }
-});
-
-UserRouter.post("/professor/register", async (request, response) => {
-    try {
-        const token = request.cookies.access_token;
-
-        if (!token) return response.status(403).send("Unauthorized access");
-        const payload = await SessionManager.verifyToken(token);
-
-        if (payload.usertype !== "director")
-            return response.status(403).send("Unauthorized access");
-
-        const { email } = request.body;
-
-        if (!email) return response.status(400).send("Error");
-        await registerProfessor({
-            email,
-            userRepository: MySQLUserRepository,
-        });
-
-        return response.status(200).send("Registered successfully");
-    } catch (error) {
-        console.error(error);
-        return response.status(400).send("Register failed");
-    }
-});
-
-UserRouter.post("/register", async (request, response) => {
-    const { email, password } = request.body;
-
-    if (!email || !password) return response.status(400).send("Error");
-
-    try {
-        await registerUser({
-            userCredentials: { email, password },
-            userRepository: MySQLUserRepository,
-            generateUsername: () => generateUsername("-", 2, 15),
-            passwordEncrypter: PasswordEncrypter,
-        });
-
-        return response.send("User registered successfully");
-    } catch (error) {
-        console.error(error);
-        return response.status(400).send("Error");
-    }
-});
-
-UserRouter.get("/validate", async (request, response) => {
-    const token = request.cookies.access_token;
-
-    if (!token) return response.status(403).send("Unauthorized access");
-
-    try {
-        const payload = await SessionManager.verifyToken(token);
-
-        return response.json({
-            email: payload.email,
-            usertype: payload.usertype,
-        });
-    } catch (error) {
-        console.error(error);
-        return response.status(400).send("Error");
-    }
-});
-
-UserRouter.post("/login", async (request, response) => {
-    const { email, password } = request.body;
-
-    if (!email || !password) return response.status(400).send("Error");
-
-    if (email === ADMIN_EMAIL && password === ADMIN_PASSWORD) {
-        const token = await SessionManager.generateToken({
-            email,
-            usertype: "admin",
-        });
-
-        return response
-            .cookie("access_token", token, {
-                secure: process.env.NODE_ENV === "production",
-            })
-            .send("Logged in successfully");
-    }
-
-    try {
         const user = await loginUser({
             userCredentials: { email, password },
             userRepository: MySQLUserRepository,
@@ -158,20 +65,71 @@ UserRouter.post("/login", async (request, response) => {
         console.error(error);
         return response.status(400).send("Log in failed");
     }
-});
+}
 
-UserRouter.post("/director/register", async (request, response) => {
+async function validateHandler(request, response) {
     try {
         const token = request.cookies.access_token;
-
         if (!token) return response.status(403).send("Unauthorized access");
+
+        const payload = await SessionManager.verifyToken(token);
+        return response.json({
+            email: payload.email,
+            usertype: payload.usertype,
+        });
+    } catch (error) {
+        console.error(error);
+        return response.status(400).send("Error");
+    }
+}
+
+async function registerHandler(request, response) {
+    try {
+        const { email, password } = request.body;
+        if (!email || !password) return response.status(400).send("Error");
+
+        await registerUser({
+            userCredentials: { email, password },
+            userRepository: MySQLUserRepository,
+            generateUsername: () => generateUsername("-", 2, 15),
+            passwordEncrypter: PasswordEncrypter,
+        });
+
+        return response.send("User registered successfully");
+    } catch (error) {
+        console.error(error);
+        return response.status(400).send("Error");
+    }
+}
+
+async function getDirectorsHandler(request, response) {
+    try {
+        const token = request.cookies.access_token;
+        if (!token) return response.status(403).send("Unauthorized access");
+
         const payload = await SessionManager.verifyToken(token);
 
         if (payload.usertype !== "admin")
             return response.status(403).send("Unauthorized access");
+        const directorList = await MySQLUserRepository.listDirectors();
+
+        return response.json(directorList);
+    } catch (error) {
+        console.log(error);
+        return response.status(400).send("Error");
+    }
+}
+
+async function registerDirectorHandler(request, response) {
+    try {
+        const token = request.cookies.access_token;
+        if (!token) return response.status(403).send("Unauthorized access");
+
+        const payload = await SessionManager.verifyToken(token);
+        if (payload.usertype !== "admin")
+            return response.status(403).send("Unauthorized access");
 
         const { email } = request.body;
-
         if (!email) return response.status(400).send("Error");
 
         await registerDirector({
@@ -184,6 +142,47 @@ UserRouter.post("/director/register", async (request, response) => {
         console.error(error);
         return response.status(400).send("Register failed");
     }
-});
+}
+
+async function getProfessorsHandler(request, response) {
+    try {
+        const token = request.cookies.access_token;
+        if (!token) return response.status(403).send("Unauthorized access");
+
+        const payload = await SessionManager.verifyToken(token);
+        if (payload.usertype !== "director")
+            return response.status(403).send("Unauthorized access");
+        const professorList = await MySQLUserRepository.listProfessors();
+
+        return response.json(professorList);
+    } catch (error) {
+        console.log(error);
+        return response.status(400).send("Error");
+    }
+}
+
+async function registerProfessorHandler(request, response) {
+    try {
+        const token = request.cookies.access_token;
+        if (!token) return response.status(403).send("Unauthorized access");
+
+        const payload = await SessionManager.verifyToken(token);
+        if (payload.usertype !== "director")
+            return response.status(403).send("Unauthorized access");
+
+        const { email } = request.body;
+
+        if (!email) return response.status(400).send("Error");
+        await registerProfessor({
+            email,
+            userRepository: MySQLUserRepository,
+        });
+
+        return response.status(200).send("Registered successfully");
+    } catch (error) {
+        console.error(error);
+        return response.status(400).send("Register failed");
+    }
+}
 
 export { UserRouter };
